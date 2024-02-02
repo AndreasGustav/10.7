31b1af0ba6ca9e969e90040ee2d077fd9ab75bf7
AndreasGustav <111876335+AndreasGustav@users.noreply.github.com>
2024-02-02 10:25:17 +0100

    Initial commit

---
+++ 10.7/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10._7
+{
+    public partial class Form1 : Form
+    {
+        Random Slump = new Random();
+        int antal = 5;
+        int[] tärningar = new int[5];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKasta_Click(object sender, EventArgs e)
+        {
+            
+            
+            kastaTärningar(tärningar, 5);
+            visaTärningar(tärningar, 5);
+        }
+        void kastaTärningar(int[] tärningar, int antal) 
+        {
+            for (int i=0; i < antal; i++)
+            {
+                int antalPrickar = Slump.Next(1, 7); // Slumpar mellan sifforna 1 till 6 åt varje tärning
+                tärningar[i] = antalPrickar;
+            }
+        }
+        void visaTärningar(int[] tärningar, int antal)
+        {
+            tbxResultat.Text = "";
+            for (int i=0;i < antal; i++) 
+            {
+                tbxResultat.AppendText("Tärning " + (i + 1) + ": " + tärningar[i] + "\r\n");
+            }
+        }
+    }
+}
